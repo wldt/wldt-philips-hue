49ff01cefe43e3eaaac9400b7ea4358a80913dfb
Marco Picone <marcopicone@Marcos-MacBook-Pro.local>
2020-07-30 14:56:31 +0200

    Initial Import

---
+++ README.md
@@ -0,0 +1,3 @@
+# Philips Hue Digital Twin Example - WhiteLabel Digital Twin Framework - Core Library
+
+This project shows how to implement a Digital Twin for Philips Hue Light Bulbs trough the WLDT Framework.
---
+++ src/main/java/it/unimore/iot/wldt/example/philips/WldtPhilipsHueLightProcess.java
@@ -0,0 +1,48 @@
+package it.unimore.iot.wldt.example.philips;
+
+import it.unimore.dipi.iot.wldt.engine.WldtConfiguration;
+import it.unimore.dipi.iot.wldt.engine.WldtEngine;
+import it.unimore.dipi.iot.wldt.exception.WldtConfigurationException;
+import it.unimore.iot.wldt.example.philips.connector.PhilipsHueLightWorker;
+import it.unimore.iot.wldt.example.philips.config.PhilipsHueLightWorkerConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Author: Marco Picone, Ph.D. (marco.picone@unimore.it)
+ * Date: 24/03/2020
+ * Project: White Label Digital Twin - Java Framework
+ */
+public class WldtPhilipsHueLightProcess {
+
+    private static final String TAG = "[WLDT-WldtPhilipsHueLightProcess]";
+
+    private static final Logger logger = LoggerFactory.getLogger(WldtPhilipsHueLightProcess.class);
+
+    public static void main(String[] args)  {
+
+        try{
+
+            logger.info("{} Initializing WLDT-Engine ... ", TAG);
+
+            //Manual creation of the WldtConfiguration
+            WldtConfiguration wldtConfiguration = new WldtConfiguration();
+            wldtConfiguration.setDeviceNameSpace("it.unimore.dipi.things");
+            wldtConfiguration.setWldtBaseIdentifier("wldt");
+            wldtConfiguration.setWldtStartupTimeSeconds(10);
+            wldtConfiguration.setApplicationMetricsEnabled(false);
+
+            PhilipsHueLightWorkerConfiguration philipsHueLightWorkerConfiguration = new PhilipsHueLightWorkerConfiguration();
+            philipsHueLightWorkerConfiguration.setBridgeAddress("192.168.1.230");
+            philipsHueLightWorkerConfiguration.setUsername("CHANGE_WITH_YOUR_USERNAME");
+
+            WldtEngine wldtEngine = new WldtEngine(wldtConfiguration);
+            wldtEngine.addNewWorker(new PhilipsHueLightWorker(wldtEngine.getWldtId(), philipsHueLightWorkerConfiguration));
+            wldtEngine.startWorkers();
+
+        }catch (Exception | WldtConfigurationException e){
+            e.printStackTrace();
+        }
+    }
+
+}
